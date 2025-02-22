e390d4bbfd00bf6374532347e50b9897096f1cda
hulk-higakijin <hm385.chejptks@gmail.com>
2025-02-22 18:18:27 +0900

    feat: login

--- frontend/src/app/(others)/login/page.tsx
+++ frontend/src/app/(others)/login/page.tsx
@@ -11,6 +11,9 @@ import Heading2 from '@/components/Heading/Heading2'
 import Image from 'next/image'
 import { ToastContainer, toast } from 'react-toastify'
 import { useSearchParams } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { handleLogin, loginScheme } from '@/controllers/login'
 
 // const loginSocials = [
 //   {
@@ -39,6 +42,14 @@ const PageLogin = ({}) => {
 		}
 	}, [searchParams])
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isSubmitting, isValid },
+	} = useForm({
+		resolver: zodResolver(loginScheme),
+	})
+
 	return (
 		<>
 			<header className="- mx-auto mb-14 max-w-2xl text-center">
@@ -49,33 +60,10 @@ const PageLogin = ({}) => {
 			</header>
 
 			<div className="mx-auto max-w-md space-y-6">
-				{/* <div className="grid gap-3"> */}
-				{/*   {loginSocials.map((item, index) => ( */}
-				{/*     <a */}
-				{/*       key={index} */}
-				{/*       href={item.href} */}
-				{/*       className="flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]" */}
-				{/*     > */}
-				{/*       <Image */}
-				{/*         className="flex-shrink-0" */}
-				{/*         src={item.icon} */}
-				{/*         alt={item.name} */}
-				{/*       /> */}
-				{/*       <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm"> */}
-				{/*         {item.name} */}
-				{/*       </h3> */}
-				{/*     </a> */}
-				{/*   ))} */}
-				{/* </div> */}
-				{/* OR */}
-				{/* <div className="relative text-center"> */}
-				{/*   <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900"> */}
-				{/*     OR */}
-				{/*   </span> */}
-				{/*   <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div> */}
-				{/* </div> */}
-				{/* FORM */}
-				<form className="grid grid-cols-1 gap-6" action="#" method="post">
+				<form
+					className="grid grid-cols-1 gap-6"
+					onSubmit={handleSubmit(handleLogin)}
+				>
 					<label className="block">
 						<span className="text-neutral-800 dark:text-neutral-200">
 							Email address
@@ -84,7 +72,11 @@ const PageLogin = ({}) => {
 							type="email"
 							placeholder="example@example.com"
 							className="mt-1"
+							{...register('email')}
 						/>
+						{errors.email && (
+							<span className="text-red-500">{errors.email.message}</span>
+						)}
 					</label>
 					<label className="block">
 						<span className="flex items-center justify-between text-neutral-800 dark:text-neutral-200">
@@ -93,12 +85,21 @@ const PageLogin = ({}) => {
 								Forgot password?
 							</NcLink>
 						</span>
-						<Input type="password" className="mt-1" />
+						<Input
+							type="password"
+							className="mt-1"
+							placeholder="******"
+							{...register('password')}
+						/>
+						{errors.password && (
+							<span className="text-red-500">{errors.password.message}</span>
+						)}
 					</label>
-					<ButtonPrimary type="submit">Continue</ButtonPrimary>
+					<ButtonPrimary type="submit" disabled={!isValid || isSubmitting}>
+						Continue
+					</ButtonPrimary>
 				</form>
 
-				{/* ==== */}
 				<span className="block text-center text-neutral-700 dark:text-neutral-300">
 					New user? {` `}
 					<NcLink href="/signup">Create an account</NcLink>
