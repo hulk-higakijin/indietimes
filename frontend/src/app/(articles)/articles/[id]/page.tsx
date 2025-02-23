df06d9fd8b69bd219f91c8590b3c862cd096d4bf
hulk-higakijin <hm385.chejptks@gmail.com>
2025-02-23 15:30:42 +0900

    feat: ShowArticlePage

---
+++ frontend/src/app/(articles)/articles/[id]/page.tsx
@@ -0,0 +1,36 @@
+import SingleContent from "@/app/(singles)/SingleContent"
+import SingleHeader from "@/app/(singles)/SingleHeader"
+import SingleRelatedPosts from "@/app/(singles)/SingleRelatedPosts"
+import NcImage from "@/components/NcImage/NcImage"
+
+const ShowArticlePage = () => {
+	return (
+		<div className={`nc-PageSingle pt-8 lg:pt-16`}>
+			<header className="container rounded-xl">
+				<div className="mx-auto max-w-screen-md">
+					<SingleHeader />
+				</div>
+			</header>
+
+			{/* FEATURED IMAGE */}
+			<NcImage
+				alt="single"
+				containerClassName="container my-10 sm:my-12"
+				className="w-full rounded-xl"
+				src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg"
+				width={1260}
+				height={750}
+				sizes="(max-width: 1024px) 100vw, 1280px"
+			/>
+
+      <div className="container mt-10">
+        <SingleContent />
+      </div>
+
+      {/* RELATED POSTS */}
+      <SingleRelatedPosts />
+		</div>
+	)
+}
+
+export default ShowArticlePage
